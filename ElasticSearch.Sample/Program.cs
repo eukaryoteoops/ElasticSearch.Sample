3f23507689fdcf610d9abdc1474023ee557415df
TonyChang <tony.chang@run-cloud.com.tw>
2019-12-16 12:00:06 +0800

    Init.

---
+++ ElasticSearch.Sample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using Serilog;
+using Serilog.Filters;
+using Serilog.Formatting.Elasticsearch;
+using Serilog.Sinks.Elasticsearch;
+
+namespace ElasticSearch.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .Enrich.FromLogContext()
+                .Filter.ByExcluding(Matching.FromSource("Microsoft"))
+                .WriteTo.RollingFile($"{Directory.GetCurrentDirectory()}/Log/log-{{Date}}.txt")
+                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
+                {
+                    AutoRegisterTemplate = true,
+                    AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv6,
+                    IndexFormat = "serilog-{0:yyyyMMdd}",
+                    TypeName = "video_api"
+                })
+                .WriteTo.Console(new ElasticsearchJsonFormatter())
+                .CreateLogger();
+            Log.Debug("something.");
+            Console.ReadKey();
+        }
+    }
+}
